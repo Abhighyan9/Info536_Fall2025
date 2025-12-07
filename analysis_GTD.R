9477e9e755dbf2ca686b34a6bd191611b54173c1
Abhi <abhighyan.bommerla@gmail.com>
2025-12-06 19:32:34 -0700

    Role 1: Cleaned GTD dataset and grouped by attack type

--- analysis_GTD.R
+++ analysis_GTD.R
@@ -2,6 +2,7 @@
 
 # Load necessary libraries
 library(dplyr)
+library(readr)
 library(ggplot2)
 
 # Load the dataset
@@ -10,4 +11,9 @@ gtd_data <- read.csv("globalterrorismdb_0718dist.csv",
 
 # Basic data exploration
 glimpse(gtd_data)
-head(gtd_data)
+
+gtd_clean <- gtd_data %>%
+  select(eventid, attacktype1, attacktype1_txt) %>%
+  filter(!is.na(attacktype1_txt))
+
+head(gtd_clean)
